1ca19f1b3d15077c2c876bc64dc5ded5e9b86caa
ryanhuge-ryjhen <nayreuqalam@gmail.com>
2024-08-24 22:39:23 +0800

    created basic express server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-import { add } from "@/test/math";
+import express from "express";
+const app = express();
 
-const a = 6;
-const b = 7;
+app.get("/", (req, res) => {
+  res.send("<h4>Welcome to my homepage</h4>");
+});
 
-console.log("The result is:", add(a, b));
+const port = process.env.PORT || 8989;
+
+app.listen(port, () =>
+  console.log(`The application is running on port http://localhost:${port}`)
+);
